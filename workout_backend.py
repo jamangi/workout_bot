d93fd6f119a30f4430a38b3b8ba614dfb8a98ed6
tetragrey <graizhon@gmail.com>
2024-04-03 11:33:42 +0200

    Add add_scheduled_workout to workout_backend

--- workout_backend.py
+++ workout_backend.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 
 def read_json(filename):
@@ -55,6 +56,36 @@ def add_user(filename, user_id, user_nick):
         json_data = json.load(file)
 
     json_data['users'][user_id] = {'username': user_nick, 'unscheduled_workout': {}, 'scheduled_workout': {}}
+
+    with open(filename, 'w') as file:
+        json.dump(json_data, file, indent=4)
+
+
+def add_scheduled_workout(filename, user_id, workout_name, days_scheduled, muscle_group=None, weights_used=None,
+                          tutorial_url=None, img_url=None):
+    """Adds a scheduled workout to the scheduled_workout dict within the user's entry in the users dict in the json.
+
+    :param filename: (str) the JSON file where the data is stored (ends with .json)
+    :param user_id: (int) the Discord ID of the user creating the workout
+    :param workout_name: (str) the name of the new workout
+    :param days_scheduled: (list) a list of the days the workout should be done. ex: ["Monday", "Friday"]
+    :param muscle_group: (str) a description of the muscle group(s) used in this workout
+    :param weights_used: (str) a description of the weights used in this workout, if any
+    :param tutorial_url: (str) a link to a tutorial that shows how to do this workout properly
+    :param img_url: (str) a link to an image that can be used in messages about this workout
+    """
+
+    time_now = datetime.now().timestamp()
+
+    with open(filename, 'r') as file:
+        json_data = json.load(file)
+
+    json_data['users'][user_id][time_now] = {'workout_name': workout_name,
+                                             'days_scheduled': days_scheduled,
+                                             'muscle_group': muscle_group,
+                                             'weights_used': weights_used,
+                                             'tutorial_url': tutorial_url,
+                                             'img_url': img_url}
 
     with open(filename, 'w') as file:
         json.dump(json_data, file, indent=4)
